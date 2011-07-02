7c11da1eba6c70f32810c2f22a093c2bd7cad425
zcs602 <zcs602@a16bb3ac-d02b-e6f1-b3a5-af853771c6d5>
2011-07-02 02:40:21 +0000

    增加gt,tq管理模块

---
+++ scgl/Ebada.Scgl.Sbgl/UCPS_gtM.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace Ebada.Scgl.Sbgl
+{
+    public partial class UCPS_gtM : DevExpress.XtraEditors.XtraUserControl
+    {
+        public UCPS_gtM()
+        {
+            InitializeComponent();
+            ucpS_GT1.FocusedRowChanged += new Ebada.Client.SendDataEventHandler<Ebada.Scgl.Model.PS_gt>(ucpS_GT1_FocusedRowChanged);
+        }
+
+        void ucpS_GT1_FocusedRowChanged(object sender, Ebada.Scgl.Model.PS_gt obj) {
+            splitCC1.Panel2.Text = "杆塔编号：" + (obj != null ? obj.gtCode : "");
+        }
+    }
+}
