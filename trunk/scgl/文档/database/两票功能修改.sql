96b430658087f9e1b9a58ec9055cd9ab42cb5fa8
maodeng_2005@126.com <maodeng_2005@126.com@a16bb3ac-d02b-e6f1-b3a5-af853771c6d5>
2011-07-25 03:29:15 +0000

    修改两票数据库字段信息

---
+++ trunk/scgl/文档/database/两票功能修改.sql
@@ -0,0 +1,14 @@
+update dbo.LP_Temple set ParentID='6360bcc1-0483-4720-a883-14bb76fd8901',CellName='电力线路第一种工作票' 
+where  CellName='一种工作票'
+
+update dbo.LP_Temple set ParentID='c29f4b8d-94a1-4432-8153-5bdad6c8a0bb',CellName='电力线路第二种工作票' 
+where  CellName='二种工作票'
+
+update dbo.LP_Temple set ParentID='db285921-68dc-4508-b86c-4ba46c9ee2ad',CellName='电力线路倒闸操作票' 
+where  CellName='倒闸操作票'
+
+
+update dbo.LP_Temple set ParentID='733294eb-2364-473a-999a-05c59fe38027',CellName='电力线路事故应急抢修单' 
+where  CellName='应急抢修单'
+
+
