7dd33299227e9c3766771c5905eb799ee6b95e2d
liuguomin@sina.com <liuguomin@sina.com@a16bb3ac-d02b-e6f1-b3a5-af853771c6d5>
2013-09-15 13:55:23 +0000

    lgm

---
+++ trunk/scgl/Ebada.Exam/FrmE_AddQuestion.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+using Ebada.Scgl.Model;
+using Ebada.Scgl.Core;
+using Ebada.Client;
+
+namespace Ebada.Exam
+{
+    public partial class FrmE_AddQuestion : DevExpress.XtraEditors.XtraForm
+    {
+        public FrmE_AddQuestion(TurnE_R_ESetPro terew,Dictionary<string, E_QuestionBank> pd,Dictionary<string, E_QuestionBank> sd,Dictionary<string, E_QuestionBank> msd)
+        {
+            InitializeComponent();
+            ucm_EaddQuestionPD.ChangeDic += new Ucm_EaddQuestion.ChangeObj(ucm_EaddQuestionPD_ChangeDic);
+            ucm_EaddQuestionSelect.ChangeDic += new Ucm_EaddQuestion.ChangeObj(ucm_EaddQuestionSelect_ChangeDic);
+            ucm_EaddQuestionMuSelect.ChangeDic += new Ucm_EaddQuestion.ChangeObj(ucm_EaddQuestionMuSelect_ChangeDic);
+            currtenter = terew;
+            PdDic = pd;
+            SelectDic = sd;
+            MuSelectDic = msd;
+
+            ucm_EaddQuestionPD.DesNum = currtenter.JudgeNUM;
+            ucm_EaddQuestionPD.PID = currtenter.PROID;
+            ucm_EaddQuestionPD.Type = "判断题";
+            ucm_EaddQuestionPD.eqdic = PdDic;
+
+            ucm_EaddQuestionSelect.DesNum = currtenter.SelectNUM;
+            ucm_EaddQuestionSelect.PID = currtenter.PROID;
+            ucm_EaddQuestionSelect.Type = "单项选择题";
+            ucm_EaddQuestionSelect.eqdic = SelectDic;
+
+            ucm_EaddQuestionMuSelect.DesNum = currtenter.MuSelectNUM;
+            ucm_EaddQuestionMuSelect.PID = currtenter.PROID;
+            ucm_EaddQuestionMuSelect.Type = "多项选择题";
+            ucm_EaddQuestionMuSelect.eqdic = MuSelectDic;
+        }
+
+
+        void ucm_EaddQuestionPD_ChangeDic(Dictionary<string, E_QuestionBank> dic)
+        {
+            PdDic = dic;
+            ShowSet();
+        }
+
+        void ucm_EaddQuestionSelect_ChangeDic(Dictionary<string, E_QuestionBank> dic)
+        {
+            SelectDic = dic;
+            ShowSet();
+        }
+
+        void ucm_EaddQuestionMuSelect_ChangeDic(Dictionary<string, E_QuestionBank> dic)
+        {
+            MuSelectDic = dic;
+            ShowSet();
+        }
+
+       
+
+        public  TurnE_R_ESetPro currtenter;
+
+        //试卷编号
+
+        public  Dictionary<string, E_QuestionBank> PdDic = new Dictionary<string, E_QuestionBank>();
+        public  Dictionary<string, E_QuestionBank> SelectDic = new Dictionary<string, E_QuestionBank>();
+        public  Dictionary<string, E_QuestionBank> MuSelectDic = new Dictionary<string, E_QuestionBank>();
+
+        Dictionary<string, TurnE_R_ESetPro> RealEREPDic = new Dictionary<string, TurnE_R_ESetPro>();
+        
+
+        private void InitData()
+        {
+
+            ShowSet();
+        }
+
+        /// <summary>
+        /// 显示设计题数和实际题数
+        /// </summary>
+        private void ShowSet()
+        {
+            //清空实际试题数量
+            currtenter.RealJudgeNUM = 0;
+            currtenter.RealSelectNUM = 0;
+            currtenter.RealMuSelectNUM = 0;
+            //计算实际判断题数量
+            foreach (string key in PdDic.Keys)
+            {
+                currtenter.RealJudgeNUM += 1;
+            }
+            //计算实际选择题数量
+            foreach (string key in SelectDic.Keys)
+            {
+                currtenter.RealSelectNUM += 1;
+            }
+            //计算实际多项选择题数量
+            foreach (string key in MuSelectDic.Keys)
+            {
+                currtenter.RealMuSelectNUM += 1;
+            }
+
+            List<TurnE_R_ESetPro> datalist = new List<TurnE_R_ESetPro>();
+            datalist.Add(currtenter);
+            int i = 1;
+            gridControl1.DataSource = null;
+            gridControl1.DataSource = datalist;
+            gridView1.Columns["PROID"].ColumnEdit = DicTypeHelper.E_proDicTB;
+            gridView1.Columns["PROID"].VisibleIndex = i++;
+            gridView1.Columns["JudgeNUM"].Width = 110;
+            gridView1.Columns["JudgeNUM"].VisibleIndex = i++;
+            gridView1.Columns["RealJudgeNUM"].Width = 110;
+            gridView1.Columns["RealJudgeNUM"].VisibleIndex = i++;
+            gridView1.Columns["SelectNUM"].Width = 110;
+            gridView1.Columns["SelectNUM"].VisibleIndex = i++;
+            gridView1.Columns["RealSelectNUM"].Width = 110;
+            gridView1.Columns["RealSelectNUM"].VisibleIndex = i++;
+            gridView1.Columns["MuSelectNUM"].Width = 110;
+            gridView1.Columns["MuSelectNUM"].VisibleIndex = i++;
+            gridView1.Columns["RealMuSelectNUM"].Width = 110;
+            gridView1.Columns["RealMuSelectNUM"].VisibleIndex = i++;
+        }
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            InitData();
+        }
+       
+       
+
+        /// <summary>
+        /// 确定
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            if (currtenter.RealJudgeNUM<currtenter.JudgeNUM)
+            {
+                MsgBox.ShowWarningMessageBox("判断题数量不足，请添加！");
+                return;
+            }
+            if (currtenter.RealSelectNUM < currtenter.SelectNUM)
+            {
+                MsgBox.ShowWarningMessageBox("单项选择题数量不足，请添加！");
+                return;
+            }
+            if (currtenter.RealMuSelectNUM < currtenter.MuSelectNUM)
+            {
+                MsgBox.ShowWarningMessageBox("多项选择题数量不足，请添加！");
+                return;
+            }
+            this.DialogResult = DialogResult.OK;
+        }
+
+    }
+}
---
+++ trunk/scgl/Ebada.Exam/Ucm_EaddQuestion.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Ebada.Exam
+{
+    partial class Ucm_EaddQuestion
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.groupControl1 = new DevExpress.XtraEditors.GroupControl();
+            this.groupControl2 = new DevExpress.XtraEditors.GroupControl();
+            this.ucE_QuestionBankForAdd1 = new Ebada.Exam.UCE_QuestionBankForAdd();
+            this.ucE_QuestionBankForAdd2 = new Ebada.Exam.UCE_QuestionBankForAdd();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).BeginInit();
+            this.groupControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.groupControl2)).BeginInit();
+            this.groupControl2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.groupControl1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.groupControl2);
+            this.splitContainer1.Size = new System.Drawing.Size(869, 437);
+            this.splitContainer1.SplitterDistance = 440;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // groupControl1
+            // 
+            this.groupControl1.Controls.Add(this.ucE_QuestionBankForAdd1);
+            this.groupControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupControl1.Location = new System.Drawing.Point(0, 0);
+            this.groupControl1.Name = "groupControl1";
+            this.groupControl1.Size = new System.Drawing.Size(440, 437);
+            this.groupControl1.TabIndex = 33;
+            this.groupControl1.Text = "可选试题（双击某行即可选中试题）";
+            // 
+            // groupControl2
+            // 
+            this.groupControl2.Controls.Add(this.ucE_QuestionBankForAdd2);
+            this.groupControl2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupControl2.Location = new System.Drawing.Point(0, 0);
+            this.groupControl2.Name = "groupControl2";
+            this.groupControl2.Size = new System.Drawing.Size(425, 437);
+            this.groupControl2.TabIndex = 34;
+            this.groupControl2.Text = "已选试题（双击某行即可移除试题）";
+            // 
+            // ucE_QuestionBankForAdd1
+            // 
+            this.ucE_QuestionBankForAdd1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ucE_QuestionBankForAdd1.Location = new System.Drawing.Point(2, 23);
+            this.ucE_QuestionBankForAdd1.Name = "ucE_QuestionBankForAdd1";
+            this.ucE_QuestionBankForAdd1.Size = new System.Drawing.Size(436, 412);
+            this.ucE_QuestionBankForAdd1.TabIndex = 0;
+            // 
+            // ucE_QuestionBankForAdd2
+            // 
+            this.ucE_QuestionBankForAdd2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ucE_QuestionBankForAdd2.Location = new System.Drawing.Point(2, 23);
+            this.ucE_QuestionBankForAdd2.Name = "ucE_QuestionBankForAdd2";
+            this.ucE_QuestionBankForAdd2.Size = new System.Drawing.Size(421, 412);
+            this.ucE_QuestionBankForAdd2.TabIndex = 0;
+            // 
+            // Ucm_EaddQuestion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Ucm_EaddQuestion";
+            this.Size = new System.Drawing.Size(869, 437);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).EndInit();
+            this.groupControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.groupControl2)).EndInit();
+            this.groupControl2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private DevExpress.XtraEditors.GroupControl groupControl1;
+        private DevExpress.XtraEditors.GroupControl groupControl2;
+        private UCE_QuestionBankForAdd ucE_QuestionBankForAdd1;
+        private UCE_QuestionBankForAdd ucE_QuestionBankForAdd2;
+    }
+}
