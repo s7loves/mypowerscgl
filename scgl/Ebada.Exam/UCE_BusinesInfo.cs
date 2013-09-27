d7857ef07d79af0c5af62e95b04f4e358dea439a
liuguomin <liuguomin@sina.com>
2013-09-27 13:45:48 +0000

    lgm

---
+++ scgl/Ebada.Exam/UCE_BusinesInfo.cs
@@ -0,0 +1,167 @@
+﻿/**********************************************
+系统:企业ERP
+模块:
+作者:Rabbit
+创建时间:2011-6-3
+最后一次修改:2011-6-3
+***********************************************/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+using Ebada.Client.Platform;
+
+using DevExpress.XtraGrid.Columns;
+using System.Reflection;
+using Ebada.Client;
+using DevExpress.XtraGrid.Views.Base;
+using Ebada.Scgl.Model;
+
+namespace Ebada.Exam {
+    /// <summary>
+    /// 
+    /// </summary>
+    //[ToolboxItem(false)]
+    public partial class UCE_BusinesInfo : DevExpress.XtraEditors.XtraUserControl {
+        private GridViewOperation<E_BusinesInfo> gridViewOperation;
+        private string parentID = null;
+        private E_BusinesInfo parentObj;
+        public event SendDataEventHandler<E_BusinesInfo> FocusedRowChanged;
+
+        public UCE_BusinesInfo()
+        {
+            InitializeComponent();
+            initImageList();
+            gridViewOperation = new GridViewOperation<E_BusinesInfo>(gridControl1, gridView1, barManager1,new FrmE_BusinesInfoEdit());
+            gridViewOperation.BeforeAdd += new ObjectOperationEventHandler<E_BusinesInfo>(gridViewOperation_BeforeAdd);
+            gridViewOperation.CreatingObjectEvent +=gridViewOperation_CreatingObjectEvent;
+            gridViewOperation.BeforeDelete += new ObjectOperationEventHandler<E_BusinesInfo>(gridViewOperation_BeforeDelete);
+            gridView1.FocusedRowChanged += new FocusedRowChangedEventHandler(gridView1_FocusedRowChanged);
+        }
+
+        void gridView1_FocusedRowChanged(object sender, FocusedRowChangedEventArgs e)
+        {
+            if (FocusedRowChanged != null)
+                FocusedRowChanged(gridView1, gridView1.GetFocusedRow() as E_BusinesInfo);
+        }
+
+        void gridViewOperation_BeforeDelete(object render, ObjectOperationEventArgs<E_BusinesInfo> e) {
+           
+        }
+
+        void gridViewOperation_BeforeAdd(object render, ObjectOperationEventArgs<E_BusinesInfo> e) {
+   
+        }
+        protected override void OnLoad(EventArgs e) {
+            base.OnLoad(e);
+
+            InitColumns();//初始列
+            InitData();//初始数据
+        }
+        private void initImageList() {
+            ImageList imagelist = new ImageList();
+            imagelist.ImageStream = (Ebada.Client.Resource.UCGridToolbar.UCGridToolbarImageList);
+            barManager1.Images = imagelist;
+        }
+
+        private void hideColumn(string colname) {
+            gridView1.Columns[colname].Visible = false;
+        }
+        /// <summary>
+        /// 初始化数据
+        /// </summary>
+        public void InitData() {
+            if (this.Site!=null &&this.Site.DesignMode) return;//必要的，否则设计时可能会报错
+            //需要初始化数据时在这写代码
+            RefreshData("  order by CreateTime desc");
+        }
+        /// <summary>
+        /// 初始化列,
+        /// </summary>
+        public void InitColumns() {
+
+            //需要隐藏列时在这写代码
+
+            //hideColumn("ID");
+            gridView1.Columns["Title"].Width = 200;
+            gridView1.Columns["Content"].Width = 500;
+        }
+
+        /// <summary>
+        /// 刷新数据
+        /// </summary>
+        /// <param name="slqwhere">sql where 子句 ，为空时查询全部数据</param>
+        public void RefreshData(string slqwhere) {
+            gridViewOperation.RefreshData(slqwhere);
+        }
+        /// <summary>
+        /// 封装了数据操作的对象
+        /// </summary>
+        [Browsable(false)]
+        public GridViewOperation<E_BusinesInfo> GridViewOperation {
+            get { return gridViewOperation; }
+            set { gridViewOperation = value; }
+        }
+        /// <summary>
+        /// 新建对象设置Key值
+        /// </summary>
+        /// <param name="newobj"></param>
+        void gridViewOperation_CreatingObjectEvent(E_BusinesInfo newobj) {
+            newobj.UserID = MainHelper.User.UserName;
+            newobj.CreateTime = DateTime.Now;
+
+        }
+
+        [Browsable(false)]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        public string ParentID
+        {
+            get { return parentID; }
+            set
+            {
+                parentID = value;
+                //if (!string.IsNullOrEmpty(value))
+                //{
+                //    RefreshData(" where ID='" + value + "' order by dxxh");
+                //}
+                //else
+                //{
+                //    RefreshData(" where dydj='10' order by dxxh");
+                //}
+            }
+        }
+        [Browsable(false)]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        public E_BusinesInfo ParentObj
+        {
+            get { return parentObj; }
+            set
+            {
+
+                parentObj = value;
+                if (value == null)
+                {
+                    parentID = null;
+                }
+                else
+                {
+                    ParentID = value.ID;
+                }
+            }
+        }
+
+        private void gridView1_GotFocus(object sender, EventArgs e)
+        {
+            if (gridView1.GetFocusedRow()!=null)
+            {
+                ParentObj = gridView1.GetFocusedRow() as E_BusinesInfo;
+            }
+        }
+
+       
+    }
+}
---
+++ scgl/Ebada.Scgl.Model/E_BusinesInfo.cs
@@ -0,0 +1,390 @@
+﻿/**********************************************
+这是代码自动生成的，如果重新生成，所做的改动将会丢失
+系统:Ebada物流企业ERP
+模块:系统平台
+Ebada.com 版权所有
+生成者：Rabbit
+生成时间:2013/9/27 21:18:10
+***********************************************/
+
+using System;
+using System.Threading;
+using System.ComponentModel;
+namespace Ebada.Scgl.Model
+{
+    /// <summary>
+    ///[E_BusinesInfo]业务实体类
+    ///对应表名:E_BusinesInfo
+    /// </summary>
+    [Serializable]
+    public class E_BusinesInfo
+    {
+        
+        #region Private 成员
+        private string _id=Newid(); 
+        private string _title=String.Empty; 
+        private string _content=String.Empty; 
+        private string _orgid=String.Empty; 
+        private string _other=String.Empty; 
+        private string _userid=String.Empty; 
+        private DateTime _createtime=new DateTime(1900,1,1); 
+        private int _sequence=0; 
+        private byte[] _worddata=new byte[]{}; 
+        private string _byscol1=String.Empty; 
+        private string _byscol2=String.Empty; 
+        private string _byscol3=String.Empty; 
+        private string _byscol4=String.Empty; 
+        private string _byscol5=String.Empty; 
+        private string _remark=String.Empty; 
+        private byte[] _rowversion=new byte[]{};   
+        #endregion
+  
+  
+        #region Public 成员
+   
+        /// <summary>
+        /// 属性名称：ID
+        /// 属性描述：
+        /// 字段信息：[ID],nvarchar
+        /// </summary>
+        [Browsable(false)]
+        [DisplayNameAttribute("")]
+        public string ID
+        {
+            get { return _id; }
+            set
+            {			
+                if(value==null)return;
+                if( value.ToString().Length > 200)
+                throw new Exception("[]长度不能大于200!");
+                if (_id as object == null || !_id.Equals(value))
+                {
+                    _id = value;
+                }
+            }			 
+        }
+  
+        /// <summary>
+        /// 属性名称：Title
+        /// 属性描述：标题
+        /// 字段信息：[Title],nvarchar
+        /// </summary>
+        [DisplayNameAttribute("标题")]
+        public string Title
+        {
+            get { return _title; }
+            set
+            {			
+                if(value==null)return;
+                if( value.ToString().Length > 50)
+                throw new Exception("[标题]长度不能大于50!");
+                if (_title as object == null || !_title.Equals(value))
+                {
+                    _title = value;
+                }
+            }			 
+        }
+  
+        /// <summary>
+        /// 属性名称：Content
+        /// 属性描述：内容
+        /// 字段信息：[Content],nvarchar
+        /// </summary>
+        [DisplayNameAttribute("内容")]
+        public string Content
+        {
+            get { return _content; }
+            set
+            {			
+                if(value==null)return;
+                if( value.ToString().Length > 500)
+                throw new Exception("[内容]长度不能大于500!");
+                if (_content as object == null || !_content.Equals(value))
+                {
+                    _content = value;
+                }
+            }			 
+        }
+  
+        /// <summary>
+        /// 属性名称：OrgID
+        /// 属性描述：机构
+        /// 字段信息：[OrgID],nvarchar
+        /// </summary>
+        [DisplayNameAttribute("机构")]
+        public string OrgID
+        {
+            get { return _orgid; }
+            set
+            {			
+                if(value==null)return;
+                if( value.ToString().Length > 50)
+                throw new Exception("[机构]长度不能大于50!");
+                if (_orgid as object == null || !_orgid.Equals(value))
+                {
+                    _orgid = value;
+                }
+            }			 
+        }
+  
+        /// <summary>
+        /// 属性名称：Other
+        /// 属性描述：其它
+        /// 字段信息：[Other],nvarchar
+        /// </summary>
+        [DisplayNameAttribute("其它")]
+        public string Other
+        {
+            get { return _other; }
+            set
+            {			
+                if(value==null)return;
+                if( value.ToString().Length > 50)
+                throw new Exception("[其它]长度不能大于50!");
+                if (_other as object == null || !_other.Equals(value))
+                {
+                    _other = value;
+                }
+            }			 
+        }
+  
+        /// <summary>
+        /// 属性名称：UserID
+        /// 属性描述：发布人
+        /// 字段信息：[UserID],nvarchar
+        /// </summary>
+        [DisplayNameAttribute("发布人")]
+        public string UserID
+        {
+            get { return _userid; }
+            set
+            {			
+                if(value==null)return;
+                if( value.ToString().Length > 50)
+                throw new Exception("[发布人]长度不能大于50!");
+                if (_userid as object == null || !_userid.Equals(value))
+                {
+                    _userid = value;
+                }
+            }			 
+        }
+  
+        /// <summary>
+        /// 属性名称：CreateTime
+        /// 属性描述：发布时间
+        /// 字段信息：[CreateTime],datetime
+        /// </summary>
+        [DisplayNameAttribute("发布时间")]
+        public DateTime CreateTime
+        {
+            get { return _createtime; }
+            set
+            {			
+                if (_createtime as object == null || !_createtime.Equals(value))
+                {
+                    _createtime = value;
+                }
+            }			 
+        }
+  
+        /// <summary>
+        /// 属性名称：Sequence
+        /// 属性描述：序号
+        /// 字段信息：[Sequence],int
+        /// </summary>
+        [DisplayNameAttribute("序号")]
+        public int Sequence
+        {
+            get { return _sequence; }
+            set
+            {			
+                if (_sequence as object == null || !_sequence.Equals(value))
+                {
+                    _sequence = value;
+                }
+            }			 
+        }
+  
+        /// <summary>
+        /// 属性名称：WordData
+        /// 属性描述：
+        /// 字段信息：[WordData],image
+        /// </summary>
+        [DisplayNameAttribute("")]
+        public byte[] WordData
+        {
+            get { return _worddata; }
+            set
+            {			
+                if (_worddata as object == null || !_worddata.Equals(value))
+                {
+                    _worddata = value;
+                }
+            }			 
+        }
+
+      
+
+        /// <summary>
+        /// 属性名称：BySCol1
+        /// 属性描述：备用1
+        /// 字段信息：[BySCol1],nvarchar
+        /// </summary>
+        [Browsable(false)]
+        [DisplayNameAttribute("备用1")]
+        public string BySCol1
+        {
+            get { return _byscol1; }
+            set
+            {
+                if (value == null) return;
+                if (value.ToString().Length > 200)
+                    throw new Exception("[备用1]长度不能大于200!");
+                if (_byscol1 as object == null || !_byscol1.Equals(value))
+                {
+                    _byscol1 = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 属性名称：BySCol2
+        /// 属性描述：备用2
+        /// 字段信息：[BySCol2],nvarchar
+        /// </summary>
+        [Browsable(false)]
+        [DisplayNameAttribute("备用2")]
+        public string BySCol2
+        {
+            get { return _byscol2; }
+            set
+            {
+                if (value == null) return;
+                if (value.ToString().Length > 200)
+                    throw new Exception("[备用2]长度不能大于200!");
+                if (_byscol2 as object == null || !_byscol2.Equals(value))
+                {
+                    _byscol2 = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 属性名称：BySCol3
+        /// 属性描述：备用3
+        /// 字段信息：[BySCol3],nvarchar
+        /// </summary>
+        [Browsable(false)]
+        [DisplayNameAttribute("备用3")]
+        public string BySCol3
+        {
+            get { return _byscol3; }
+            set
+            {
+                if (value == null) return;
+                if (value.ToString().Length > 200)
+                    throw new Exception("[备用3]长度不能大于200!");
+                if (_byscol3 as object == null || !_byscol3.Equals(value))
+                {
+                    _byscol3 = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 属性名称：BySCol4
+        /// 属性描述：备用4
+        /// 字段信息：[BySCol4],nvarchar
+        /// </summary>
+        [Browsable(false)]
+        [DisplayNameAttribute("备用4")]
+        public string BySCol4
+        {
+            get { return _byscol4; }
+            set
+            {
+                if (value == null) return;
+                if (value.ToString().Length > 200)
+                    throw new Exception("[备用4]长度不能大于200!");
+                if (_byscol4 as object == null || !_byscol4.Equals(value))
+                {
+                    _byscol4 = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 属性名称：BySCol5
+        /// 属性描述：备用5
+        /// 字段信息：[BySCol5],nvarchar
+        /// </summary>
+        [Browsable(false)]
+        [DisplayNameAttribute("备用5")]
+        public string BySCol5
+        {
+            get { return _byscol5; }
+            set
+            {
+                if (value == null) return;
+                if (value.ToString().Length > 200)
+                    throw new Exception("[备用5]长度不能大于200!");
+                if (_byscol5 as object == null || !_byscol5.Equals(value))
+                {
+                    _byscol5 = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 属性名称：Remark
+        /// 属性描述：备注
+        /// 字段信息：[Remark],nvarchar
+        /// </summary>
+        [Browsable(false)]
+        [DisplayNameAttribute("备注")]
+        public string Remark
+        {
+            get { return _remark; }
+            set
+            {
+                if (value == null) return;
+                if (value.ToString().Length > 200)
+                    throw new Exception("[备注]长度不能大于200!");
+                if (_remark as object == null || !_remark.Equals(value))
+                {
+                    _remark = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 属性名称：RowVersion
+        /// 属性描述：时间戳
+        /// 字段信息：[RowVersion],timestamp
+        /// </summary>
+        [Browsable(false)]
+        [DisplayNameAttribute("时间戳")]
+        public byte[] RowVersion
+        {
+            get { return _rowversion; }
+            set
+            {
+                if (_rowversion as object == null || !_rowversion.Equals(value))
+                {
+                    _rowversion = value;
+                }
+            }
+        }
+        #endregion 
+  
+        #region 方法
+        public static string Newid(){
+            return DateTime.Now.ToString("yyyyMMddHHmmssffffff");
+        }
+        public string CreateID(){
+            Thread.Sleep(new TimeSpan(100000));//0.1毫秒
+            return DateTime.Now.ToString("yyyyMMddHHmmssffffff");
+        }
+        #endregion		
+    }	
+}
